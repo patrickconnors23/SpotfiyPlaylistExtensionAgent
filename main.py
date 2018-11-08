959e62bffa07b17212c15f47d3cd624af0b24d1c
Patrick <patrickconnors@college.harvard.edu>
2018-11-07 20:54:07 -0500

    Testing branching

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import accuracy_score, r2_score
+import matplotlib
+import matplotlib.pyplot as plt
+
+print(np.arange(1, 100))
+
