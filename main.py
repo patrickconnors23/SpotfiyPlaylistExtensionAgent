d103d030563098b37c2e693be2867e1c6f7ba001
Patrick <patrickconnors@college.harvard.edu>
2018-11-11 19:55:00 -0500

    Read data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import json
+import pprint as pp
 from sklearn import metrics, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LinearRegression
@@ -9,15 +11,19 @@ from sklearn.metrics import accuracy_score, r2_score
 import matplotlib
 import matplotlib.pyplot as plt
 
+PATH = "../data/data/mpd.slice.96000-96999.json"
 
 class exploreData():
-    def __init__(self):
-        self.data = pd.DataFrame()
+    def __init__(self, path):
+        self.data = self.readData(path)
     
-    def readData(self, data):
-        return
+    def readData(self, path):
+        with open(path) as f:
+            data = json.load(f)
+            playlists = data["playlists"]
+        return pd.DataFrame(playlists)
 
 if __name__ == "__main__":
-    x = exploreData()
+    x = exploreData(PATH)
     print(x)
 
