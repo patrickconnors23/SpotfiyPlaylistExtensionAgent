06807165afe0a25a3eaf01eaac64c5ec1ab07f6f
patrickconnors23 <patconnors23@gmail.com>
2018-11-28 21:20:47 -0500

    Keyword visualization

--- util/vis.py
+++ util/vis.py
@@ -4,20 +4,20 @@ import numpy as np
 import pandas as pd
 
 
-"""
-Helper used to plot histogram
-"""
 def plotHist(ax, title, xlabel, ylabel, data):
+    """
+    Helper used to plot histogram
+    """
     ax.hist(data, density=True)
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-"""
-Display the number of playlist inclusions for
-X most popular artists
-"""
 def displayPopularArtists(df, lim=100):
+    """
+    Display the number of playlist inclusions for
+    X most popular artists
+    """
     # Initialize dictionary of artist names
     artists = {}
     for playlist in df.tracks:
@@ -54,16 +54,39 @@ def displayPopularArtists(df, lim=100):
 Displays bar chart of most common keywords
 used in playlist names
 """
-def displayMostCommonKeyWord():
-    count = 0
+def displayMostCommonKeyWord(df):
+    playlist_names = df["name"]
+    dic = {}
+    
+    # Break into keywords
+    keywords = [list(filter(None,name.split(" "))) for name in playlist_names]
+    flat_list = [item for sublist in keywords for item in sublist]
+
+    # Sort dict and break up into consumable form
+    key_dict = dict((x,flat_list.count(x)) for x in set(flat_list))
+    key_dict = sorted(key_dict.items(),key=lambda x:x[1], reverse=True)
+    word, count = zip(*key_dict[:20])
+
+    xvals = np.arange(len(word))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+    plt.bar(xvals, count)
+    plt.xticks(
+            ticks=xvals,
+            labels=word,
+            rotation="vertical",
+            fontsize=7)
+    plt.xlabel("KeyWord")
+    plt.ylabel("Frequency")
+    plt.title("Most Common Words in Playlist Titles")
+    plt.savefig("figs/kwF.png")
     pass
 
-"""
-Displays histogram of playlist lengths,
-allows us to get a sense of the size of
-a typical playlist 
-"""
 def displayPlaylistLengthDistribution(df):
+    """
+    Displays histogram of playlist lengths,
+    allows us to get a sense of the size of
+    a typical playlist 
+    """
     fig, ax = plt.subplots(1, 1, figsize=(10, 5))
     numTracks = [len(x) for x in df.tracks]
     plotHist(
